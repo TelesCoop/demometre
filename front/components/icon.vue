15f4de81e67309d374928d42f81949694624677e
Maxime Bellec <maxime@telescoop.fr>
2025-10-07 09:44:04 +0200

    feat: front: homepage: add international section

--- front/components/icon.vue
+++ front/components/icon.vue
@@ -284,6 +284,10 @@
         :fill="props.color"
       />
     </template>
+    <template v-else-if="props.name === 'external-link'">
+      <path d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V11H19L18.9999 6.413L11.2071 14.2071L9.79289 12.7929L17.5849 5H13V3H21Z"
+      :fill="props.color"></path>
+    </template>
     <template v-else-if="props.name === ''">
       <path
         d="M13 20H11V13H4V20H2V4H4V11H11V4H13V20ZM21.0005 8V20H19.0005L19 10.204L17 10.74V8.67L19.5005 8H21.0005Z"
