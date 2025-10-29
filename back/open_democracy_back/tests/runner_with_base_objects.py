d59fb187d973e66b8be655d12ac0d2d5e8914440
Marc-AntoineA <dev@marc-antoinea.fr>
2025-10-29 13:22:29 +0100

    fix: should finally fix pre-commit

--- back/open_democracy_back/tests/runner_with_base_objects.py
+++ back/open_democracy_back/tests/runner_with_base_objects.py
@@ -2,15 +2,15 @@ from django.test.runner import DiscoverRunner as BaseRunner
 from wagtail.models import Locale
 
 
-class MyMixinRunner(object):
+class MyMixinRunner:
     def setup_databases(self, *args, **kwargs):
-        temp_return = super(MyMixinRunner, self).setup_databases(*args, **kwargs)
+        temp_return = super().setup_databases(*args, **kwargs)
         Locale.objects.create(language_code="fr")
         return temp_return
 
     def teardown_databases(self, *args, **kwargs):
         # do somthing
-        return super(MyMixinRunner, self).teardown_databases(*args, **kwargs)
+        return super().teardown_databases(*args, **kwargs)
 
 
 class MyTestRunner(MyMixinRunner, BaseRunner):
