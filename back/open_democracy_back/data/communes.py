34b7dae06c576eedb88a4c2c0d94fe5b01116a7a
Maxime Bellec <maxime@telescoop.fr>
2025-11-04 15:15:30 +0100

    up

--- back/open_democracy_back/data/communes.py
+++ back/open_democracy_back/data/communes.py
@@ -3,8 +3,8 @@ from django.conf import settings
 country = settings.COUNTRY
 
 if country == "FR":
-    from .fr.communes import COMMUNES
+    from .fr.communes import COMMUNES  # noqa: F401
 elif country == "BE":
-    from .be.communes import COMMUNES
+    from .be.communes import COMMUNES  # noqa: F401
 else:
     raise NotImplementedError(f"Country {country} not implemented")
--- back/open_democracy_back/data/epci.py
+++ back/open_democracy_back/data/epci.py
@@ -3,8 +3,8 @@ from django.conf import settings
 country = settings.COUNTRY
 
 if country == "FR":
-    from .fr.epci import EPCI
+    from .fr.epci import EPCI  # noqa: F401
 elif country == "BE":
-    from .be.communes import EPCI
+    from .be.communes import EPCI  # noqa: F401
 else:
     raise NotImplementedError(f"Country {country} not implemented")
